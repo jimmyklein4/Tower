63fc381b73d6773803b994970d217e7cccbe952b
welcome-2themachine <Christopher.prevost94@gmail.com>
2016-03-16 15:33:58 -0400

    Test Commit
    This is a test commit, I doubled the size of the cube.

--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -21,7 +21,7 @@ public class Main extends SimpleApplication {
 
     @Override
     public void simpleInitApp() {
-        Box b = new Box(1, 1, 1);
+        Box b = new Box(2, 2, 2);
         Geometry geom = new Geometry("Box", b);
 
         Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
