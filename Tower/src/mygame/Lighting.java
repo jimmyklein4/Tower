5d39b9110d156c07396ec30ee7f52f11002fedcb
welcome-2themachine <Christopher.prevost94@gmail.com>
2016-03-23 10:16:11 -0400

    Reset Complete

---
+++ Tower/src/mygame/Lighting.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mygame;
+
+import com.jme3.light.AmbientLight;
+import com.jme3.math.ColorRGBA;
+
+/**
+ *
+ * @author Chris 
+ */
+public class Lighting {
+    Main msa;
+    public Lighting(){
+        
+    }
+    public Lighting(Main msa){
+        this.msa = msa;
+        AmbientLight ambient = new AmbientLight();
+        ambient.setColor(ColorRGBA.White);
+        this.msa.getRootNode().addLight(ambient);  
+    }
+}
