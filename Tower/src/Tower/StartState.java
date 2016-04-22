b82aa625509e88a5f0419bf6771ffa1b4aea967a
welcome-2themachine <Christopher.prevost94@gmail.com>
2016-04-21 21:42:16 -0400

    View controls in StartState
    Press C

--- Tower/src/Tower/StartState.java
+++ Tower/src/Tower/StartState.java
@@ -19,12 +19,16 @@ import com.jme3.ui.Picture;
  */
 public class StartState extends AbstractAppState implements ActionListener{
     
-    Picture pic;
-    Main main;
-    AppStateManager asm;
-    InputManager inputManager;
-    BitmapFont bmf;
-    BitmapText level1, level2;
+    private Picture pic;
+    private Main main;
+    private AppStateManager asm;
+    private InputManager inputManager;
+    private BitmapFont bmf;
+    private BitmapText level1, level2, viewControls,
+                       controlExit, controlQuit, controlPause,
+                       controlJump, controlMove, controlSwitch;
+    private boolean controlView = false;
+    
         
     //==========================================================================
     public void onAction(String name, boolean isPressed, float tpf) {
@@ -46,6 +50,9 @@ public class StartState extends AbstractAppState implements ActionListener{
             }
             if(name.equals("Quit")){
                 main.stop();
+            }
+            if(name.equals("ViewControls")){
+                viewControls();
             }
         }
     }
@@ -70,7 +77,8 @@ public class StartState extends AbstractAppState implements ActionListener{
         inputManager.addMapping("Quit", new KeyTrigger(KeyInput.KEY_ESCAPE));
         inputManager.addMapping("Level1", new KeyTrigger(KeyInput.KEY_1));
         inputManager.addMapping("Level2", new KeyTrigger(KeyInput.KEY_2));
-        inputManager.addListener(this, "Start", "Quit", "Level1", "Level2");
+        inputManager.addMapping("ViewControls", new KeyTrigger(KeyInput.KEY_C));
+        inputManager.addListener(this, "Start", "Quit", "Level1", "Level2", "ViewControls");
     }
     //==========================================================================
     private void initText(){
@@ -84,22 +92,87 @@ public class StartState extends AbstractAppState implements ActionListener{
         bmf = main.getAssetManager().loadFont("Interface/Fonts/Jokerman.fnt");
         level1 = new BitmapText(bmf);
         level2 = new BitmapText(bmf);
+        viewControls = new BitmapText(bmf);
+        controlExit = new BitmapText(bmf);
+        controlJump = new BitmapText(bmf);
+        controlMove = new BitmapText(bmf);
+        controlPause = new BitmapText(bmf);
+        controlQuit = new BitmapText(bmf);
+        controlSwitch = new BitmapText(bmf);
         
         level1.setColor(ColorRGBA.Black);
         level2.setColor(ColorRGBA.Black);
+        viewControls.setColor(ColorRGBA.Black);
+        controlExit.setColor(ColorRGBA.Black);
+        controlQuit.setColor(ColorRGBA.Black);
+        controlPause.setColor(ColorRGBA.Black);
+        controlJump.setColor(ColorRGBA.Black);
+        controlMove.setColor(ColorRGBA.Black);
+        controlSwitch.setColor(ColorRGBA.Black);
         
         level1.setSize(bmf.getCharSet().getRenderedSize() * Main.screenWidth / 800);
         level2.setSize(bmf.getCharSet().getRenderedSize() * Main.screenWidth / 800);
+        viewControls.setSize(bmf.getCharSet().getRenderedSize() * Main.screenWidth / 800);
+        controlExit.setSize(bmf.getCharSet().getRenderedSize() * Main.screenWidth / 800);
+        controlQuit.setSize(bmf.getCharSet().getRenderedSize() * Main.screenWidth / 800);
+        controlPause.setSize(bmf.getCharSet().getRenderedSize() * Main.screenWidth / 800);
+        controlJump.setSize(bmf.getCharSet().getRenderedSize() * Main.screenWidth / 800);
+        controlMove.setSize(bmf.getCharSet().getRenderedSize() * Main.screenWidth / 800);
+        controlSwitch.setSize(bmf.getCharSet().getRenderedSize() * Main.screenWidth / 800);
         
         level1.setText("Press [1] to start Level 1");
         level2.setText("Press [2] to start Level 2");
+        viewControls.setText("Press [C] to view controls");
+        controlExit.setText("Press [ESC] to exit");
+        controlQuit.setText("Press [Q] to quit");
+        controlPause.setText("Press [P] to pause");
+        controlJump.setText("Press [SPACE] to jump");
+        controlMove.setText("Press [WASD] to move");
+        controlSwitch.setText("Press [UP DOWN LEFT RIGHT] to move the tower");
         
         level1.setLocalTranslation(new Vector3f(0, level1.getHeight() * 2, 0));
         level2.setLocalTranslation(new Vector3f(0, level2.getHeight(), 0));
+        viewControls.setLocalTranslation(new Vector3f(0, level2.getHeight() * 3, 0));
+        controlExit.setLocalTranslation(new Vector3f(0,
+                Main.screenHeight,0));
+        controlQuit.setLocalTranslation(new Vector3f(0,
+                Main.screenHeight-controlExit.getHeight(),0));
+        controlPause.setLocalTranslation(new Vector3f(0,
+                Main.screenHeight-controlExit.getHeight()-controlQuit.getHeight(),0));
+        controlJump.setLocalTranslation(new Vector3f(0,
+                Main.screenHeight-controlExit.getHeight()-controlQuit.getHeight()
+                -controlPause.getHeight(),0));
+        controlMove.setLocalTranslation(new Vector3f(0,
+                Main.screenHeight-controlExit.getHeight()-controlQuit.getHeight()
+                -controlPause.getHeight()-controlJump.getHeight(),0));
+        controlSwitch.setLocalTranslation(new Vector3f(0,
+                Main.screenHeight-controlExit.getHeight()-controlQuit.getHeight()
+                -controlPause.getHeight()-controlJump.getHeight()-controlMove.getHeight(), 0));
         
         main.getGuiNode().attachChild(level1);
         main.getGuiNode().attachChild(level2);
+        main.getGuiNode().attachChild(viewControls);
     }
     //==========================================================================
+    private void viewControls(){
+        if(controlView){
+            controlView = false;
+            main.getGuiNode().detachAllChildren();
+            main.getGuiNode().attachChild(pic);
+            main.getGuiNode().attachChild(level1);
+            main.getGuiNode().attachChild(level2);
+            main.getGuiNode().attachChild(viewControls);
+        }
+        else{
+            controlView = true;
+            main.getGuiNode().detachAllChildren();
+            main.getGuiNode().attachChild(controlExit);
+            main.getGuiNode().attachChild(controlQuit);
+            main.getGuiNode().attachChild(controlPause);
+            main.getGuiNode().attachChild(controlJump);
+            main.getGuiNode().attachChild(controlMove);
+            main.getGuiNode().attachChild(controlSwitch);
+        }
+    }
 }
 
